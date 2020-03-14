8c882229d4a48adea503218e8f579106a291ba7d
Lane Moseley <moseley.lane@gmail.com>
2020-03-14 11:54:32 -0600

    Added sections to help page

--- help.php
+++ help.php
@@ -4,18 +4,17 @@
     <title>TODO: Help Page</title>
     <link rel="stylesheet" type="text/css" href="CSS/style.css">
 </head>
-<body>
 
 <body>
     <?php include "includes/header.html" ?>
 
     <div class="content">
         <h1>Help Header</h1>
+        <h3>Description</h3>
+        <h3>CSS Modifications</h3>
+        <h3>Usage</h3>
     </div>
 
     <?php include "includes/footer.html" ?>
-
-</body>
-
 </body>
 </html>
