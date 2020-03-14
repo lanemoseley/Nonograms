671b019f85884ddd232176a489230b101059a8b3
Lane Moseley <moseley.lane@gmail.com>
2020-03-13 23:39:38 -0600

    Added upload option

--- fileManagement/index.php
+++ fileManagement/index.php
@@ -1,4 +1,7 @@
-<!DOCTYPE html>
+<?php
+require_once 'upload.php';
+?>
+
 <html>
 <head lang="en">
     <meta charset="UTF-8">
@@ -12,6 +15,21 @@
 
     <div class="content">
         <h1>File Stuff Page Header</h1>
+        <h3>Uploading</h3>
+        <!--basic structure for a file upload-->
+        <?php
+        //determine what to do based on GET variables
+        if (isset($_GET['up'])) {
+            upload();
+        }
+        ?>
+        <form action="index.php?up=1" method="post" enctype="multipart/form-data">
+            Select file to upload:
+
+            <!--name and id are used as identifiers of the file on the php side-->
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload" name="submit">
+        </form>
     </div>
 
     <?php include "../includes/footer.html" ?>
